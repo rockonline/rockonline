2a9782f95472c0b305afbcc950854635d3fa417c
rockonline <rockonline@outlook.com.br>
2016-03-30 11:00:57 -0300

    Create extension.js

---
+++ extension.js
@@ -0,0 +1,132 @@
+(function () {
+
+    //Change this to your GitHub username so you don't have to modify so many things.
+    var fork = "NOOBinho";
+
+    //Define our function responsible for extending the bot.
+    function extend() {
+        //If the bot hasn't been loaded properly, try again in 1 second(s).
+        if (!window.bot) {
+            return setTimeout(extend, 1 * 1000);
+        }
+
+        //Precaution to make sure it is assigned properly.
+        var bot = window.bot;
+
+        //Load custom settings set below
+        bot.retrieveSettings();
+
+        /*
+         Extend the bot here, either by calling another function or here directly.
+         Model code for a bot command:
+
+         bot.commands.commandCommand = {
+         	command: 'cmd',
+         	rank: 'user/bouncer/mod/manager',
+         	type: 'startsWith/exact',
+         	functionality: function(chat, cmd){
+        		if(this.type === 'exact' && chat.message.length !== cmd.length) return void (0);
+         		if( !bot.commands.executable(this.rank, chat) ) return void (0);
+         		else{
+         		//Commands functionality goes here.
+         		}
+        	 }
+         }
+
+         */
+         spam: [
+                'hueh', 'hu3', 'brbr', 'heu', 'brbr', 'kkkk', 'spoder', 'mafia', 'zuera', 'zueira',
+                'zueria', 'aehoo', 'aheu', 'alguem', 'algum', 'brazil', 'zoeira', 'fuckadmins', 'affff', 'vaisefoder', 'huenaarea',
+                'hitler', 'ashua', 'ahsu', 'ashau', 'lulz', 'huehue', 'hue', 'huehuehue', 'merda', 'pqp', 'puta', 'mulher', 'pula', 'retarda', 'caralho', 'filha', 'ppk',
+                'gringo', 'fuder', 'foder', 'hua', 'ahue', 'modafuka', 'modafoka', 'mudafuka', 'mudafoka', 'ooooooooooooooo', 'foda'
+                
+            ],
+
+        bot.commands.baconCommand = {
+            command: 'bacon',  //The command to be called. With the standard command literal this would be: !bacon
+            rank: 'user', //Minimum user permission to use the command
+            type: 'exact', //Specify if it can accept variables or not (if so, these have to be handled yourself through the chat.message
+            functionality: function (chat, cmd) {
+                if (this.type === 'exact' && chat.message.length !== cmd.length) return void (0);
+                if (!bot.commands.executable(this.rank, chat)) return void (0);
+                else {
+                    API.sendChat("/me Bacon!!!");
+                }
+            }
+        };
+
+        //Load the chat package again to account for any changes
+        bot.loadChat();
+
+    }
+
+    //Change the bots default settings and make sure they are loaded on launch
+
+    localStorage.setItem("basicBotsettings", JSON.stringify({
+        botName: "RockOnline", //Define a name for your bot
+        language: "Portuguese",
+        chatLink: "https://raw.githubusercontent.com/rockonline/rockonline/master/pt-br",
+        startupCap: 100, // 1-200
+        startupVolume: 50, // 0-100
+        startupEmoji: true, // true or false
+        autowoot: true,
+        autoskip: true,
+        smartSkip: true,
+        cmdDeletion: true,
+        maximumAfk: 120,
+        afkRemoval: true,
+        maximumDc: 60,
+        bouncerPlus: true,
+        blacklistEnabled: true,
+        lockdownEnabled: false,
+        lockGuard: false,
+        maximumLocktime: 10,
+        cycleGuard: true,
+        maximumCycletime: 10,
+        voteSkip: false,
+        voteSkipLimit: 10,
+        historySkip: false,
+        timeGuard: true,
+        maximumSongLength: 10,
+        autodisable: true,
+        commandCooldown: 30,
+        usercommandsEnabled: true,
+        skipPosition: 1,
+        skipReasons: [
+            ["tema", "A música não se encaixa nos padrões da sala. "],
+                ["op", "Essa música está na lista OP. "],
+                ["historico", "A música ainda está no histórico. "],
+                ["mix", "Você tocou um mix (muito longo) - não permitido. "],
+                ["som", "A música que você tocou tinha má qualidade ou estava sem som. "],
+                ["nsfw", "A música que você tocou é NSFW (impróprio). "],
+                ["indisponivel", "A música que você tocou está indisponível. "]
+        ],
+        afkpositionCheck: 15,
+        afkRankCheck: "ambassador",
+        motdEnabled: false,
+        motdInterval: 5,
+        motd: null,
+        filterChat: true,
+        etaRestriction: false,
+        welcome: true,
+        opLink: null,
+        rulesLink: null,
+        themeLink: null,
+        fbLink: "https://www.facebook.com/groups/rockonlineplugdj/",
+        youtubeLink: null,
+        website: null,
+        intervalMessages: [],
+        messageInterval: 5,
+        songstats: false,
+        commandLiteral: "!",
+        blacklists: {
+            NSFW: "https://rawgit.com/" + fork + "/basicBot-customization/master/blacklists/NSFWlist.json",
+            OP: "https://rawgit.com/" + fork + "/basicBot-customization/master/blacklists/OPlist.json",
+            BANNED: "https://rawgit.com/" + fork + "/basicBot-customization/master/blacklists/BANNEDlist.json"
+        }
+    }));
+
+    //Start the bot and extend it when it has loaded.
+    $.getScript("https://rawgit.com/Yemasthui/basicBot/master/basicBot.js", extend);
+
+}).call(this);
